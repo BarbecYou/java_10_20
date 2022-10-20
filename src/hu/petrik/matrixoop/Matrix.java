f50af1d8f04dfb777792f3d2de8d040a63dcbe60
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-20 09:04:40 +0200

    getElsoPozitivIndex kesz

--- src/hu/petrik/matrixoop/Matrix.java
+++ src/hu/petrik/matrixoop/Matrix.java
@@ -17,14 +17,14 @@ public class Matrix {
 
         for (int i = 0; i < this.m.length; i++) {
             for (int j = 0; j < this.m[i].length; j++) {
-                this.m[i][j] = rnd.nextInt(150)-50;
+                this.m[i][j] = rnd.nextInt(150) - 50;
             }
         }
     }
 
     public int getElemekOsszege() {
         int osszeg = 0;
-        for (int[] sor : this.m){
+        for (int[] sor : this.m) {
             for (int elem : sor) {
                 osszeg += elem;
             }
@@ -32,11 +32,11 @@ public class Matrix {
         return osszeg;
     }
 
-    public int getPozitivElemekSzama(){
+    public int getPozitivElemekSzama() {
         int darabSzam = 0;
-        for (int[] sor : this.m){
-            for (int elem : sor){
-                if (elem > 0){
+        for (int[] sor : this.m) {
+            for (int elem : sor) {
+                if (elem > 0) {
                     darabSzam++;
                 }
             }
@@ -44,11 +44,11 @@ public class Matrix {
         return darabSzam;
     }
 
-    public int getLegnagyobbElem(){
-        int legnagyobb = 0;
-        for (int[] sor : this.m){
+    public int getLegnagyobbElem() {
+        int legnagyobb = this.m[0][0];
+        for (int[] sor : this.m) {
             for (int elem : sor) {
-                if (elem > legnagyobb){
+                if (elem > legnagyobb) {
                     legnagyobb = elem;
                 }
             }
@@ -56,12 +56,24 @@ public class Matrix {
         return legnagyobb;
     }
 
+    public int getElsoPozitivIndex() {
+        int sorIndex = 0;
+        int oszlopIndex = 0;
+        while (sorIndex < this.m.length && this.m[sorIndex][oszlopIndex] < 1){
+            while (oszlopIndex < this.m[sorIndex].length && this.m[sorIndex][oszlopIndex] < 1){
+                oszlopIndex++;
+            }
+            sorIndex++;
+        }
+        return this.m[sorIndex][oszlopIndex];
+    }
+
     @Override
     public String toString() {
         StringBuilder sb = new StringBuilder();
 
         for (int[] sor : this.m) {
-            for (int elem : sor){
+            for (int elem : sor) {
                 sb.append(String.format("%3d ", elem));
             }
             sb.append(System.lineSeparator());
