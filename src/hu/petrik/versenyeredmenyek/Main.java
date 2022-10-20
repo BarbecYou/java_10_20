ea82a0707dbc6dce960391feb6d671fd068bcaca
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-20 11:15:06 +0200

    versenyzok szama kesz

--- src/hu/petrik/versenyeredmenyek/Main.java
+++ src/hu/petrik/versenyeredmenyek/Main.java
@@ -11,8 +11,41 @@ public class Main {
         String fileName = "eredmenyek.txt";
         try {
             beolvas(fileName);
+            kiiras();
+            System.out.println("Sportágak száma: " + getSportagakSzama());
+            System.out.printf("Az olimpián %d versenyző vett részt\n", getVersenyzokSzama());
         } catch (FileNotFoundException e){
             System.out.printf("Nem található a file");
+        }
+    }
+
+    private static int getVersenyzokSzama() {
+        List<String> versenyzok = new ArrayList<>();
+        for (Map.Entry<String, List<Eredmeny>> entry : sportagEredmenyek.entrySet()){
+            List<Eredmeny> eredmenyek = entry.getValue();
+            for (Eredmeny e : eredmenyek){
+                if (!versenyzok.contains(e.getNev())){
+                    versenyzok.add(e.getNev());
+                }
+            }
+        }
+        return versenyzok.size();
+    }
+
+    private static int getSportagakSzama(){
+        return sportagEredmenyek.keySet().size();
+    }
+
+    private static void kiiras() {
+        for (Map.Entry<String, List<Eredmeny>> entry : sportagEredmenyek.entrySet()){
+            String sportag = entry.getKey();
+            List<Eredmeny> eredmenyek = entry.getValue();
+
+            System.out.println(sportag);
+
+            for (Eredmeny e : eredmenyek){
+                System.out.println("\t" + e);
+            }
         }
     }
 
