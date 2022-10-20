e67ebbccd2ba08d1a411222f812d599dd23429ec
Füredi Dániel <daniel.furedi03@gmail.com>
2022-10-20 10:54:46 +0200

    beolvas, hashmap letrehozasa

--- src/hu/petrik/versenyeredmenyek/Main.java
+++ src/hu/petrik/versenyeredmenyek/Main.java
@@ -1,7 +1,32 @@
 package hu.petrik.versenyeredmenyek;
 
-public class Main {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
 
+public class Main {
+    private static Map<String, List<Eredmeny>> sportagEredmenyek = new HashMap<>();
+
+    public static void main(String[] args) {
+        String fileName = "eredmenyek.txt";
+        try {
+            beolvas(fileName);
+        } catch (FileNotFoundException e){
+            System.out.printf("Nem található a file");
+        }
+    }
+
+    private static void beolvas(String fileName) throws FileNotFoundException {
+        Scanner file = new Scanner(new File(fileName));
+        while (file.hasNext()) {
+            String[] sor = file.nextLine().split(" ");
+            String sportag = sor[0];
+            String reszIdo = sor[1];
+            String nev = sor[2] + " " + sor[3];
+            sportagEredmenyek.putIfAbsent(sportag, new ArrayList<>());
+            Eredmeny eredmeny = new Eredmeny(reszIdo, nev);
+            sportagEredmenyek.get(sportag).add(eredmeny);
+        }
+        file.close();
     }
 }
